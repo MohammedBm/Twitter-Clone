cedc8fc187d1489ba4241e4e035875dbfb2fdbe0
Mohmed Bamhraz <mohmedbamhrz@gmail.com>
2017-11-25 18:22:32 -0800

    - update SignIn page

---
+++ mobile/src/components/SignupForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components/native'
+import { MaterialIcons } from '@expo/vector-icons';
+
+const Root = styled.View``
+const T = styled.Text``
+
+
+class SignupForm extends React.Component {
+  render() {
+    return (
+      <Root>
+        <T>SignupForm</T>
+      </Root>
+    )
+  }
+}
+
+export default SignupForm;
